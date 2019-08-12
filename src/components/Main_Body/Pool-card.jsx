da37f7d95737058f9d65ac04caa6a4c4dce09642
Stroupe79 <cstroupe79@gmail.com>
2019-08-12 15:44:13 -0500

    changed up some animations

--- src/components/Main_Body/Pool-card.jsx
+++ src/components/Main_Body/Pool-card.jsx
@@ -1,54 +1,4 @@
-// import React from "react";
-// import "./style.css";
-// import Wedding2 from "../../util/images/pool4.jpg"
-// import Pool1 from "../../util/images/pool1.jpeg"
-// import ResizeImage from 'react-resize-image'
-// import {Card, Table} from 'reactstrap'
-// import Card from '@material-ui/core/Card';
-
-// const Wedding = ({show}) => {
-//     return (
-//         <div className="react-transition swipe-right m-5" style={{animationDuration: '1s', backgroundColor: '#bef1e9'}}>
-//         <Card>
-//             <div className="m-5">
-
-      
-//         <p>Rent the barn for your wedding! Beautiful rustic location that will give you an experince you'll never forget and cherish forever! </p>
-//         <img className="wedding m-5" src="https://cdn0.weddingwire.com/img_g/ww/t30_fairview-farm.jpg"
-//         alt="Wedding Venue"
-//         />
-
-//         <Table bordered>
-//         <thead>
-//   <tr>
-//     <th>Rates</th>
-//     <th>Hours</th> 
-//   </tr>
-//   </thead>
-//   <tr>
-//     <td>$1500</td>
-//     <td>Full Day</td> 
-//   </tr>
-//   <tr>
-//     <td>$750</td>
-//     <td>Half Day</td> 
-//   </tr>
-// </Table>
-
-// </div>
-// </Card>
-// </div>
-//     );
-// }
-
-
-
-// export default Wedding
-
-
-
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -57,7 +7,6 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import "./style.css";
-import Table from './Table'
 
 
 
@@ -66,8 +15,7 @@ function PoolCard() {
 
 
   return (
-    // <div className="react-transition swipe-right m-5" style={{animationDuration: '1s'}}>
-    <Card raised='true' className="card react-transition swipe-right" style={{animationDuration: '1s'}}>
+    <Card raised='true' className="card react-transition swipe-right" style={{animationDuration: '1s', animationDelay: '1s'}}>
       <CardActionArea>
         <CardMedia
           className="media"
@@ -75,11 +23,11 @@ function PoolCard() {
           title="Pool"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
+          <Typography gutterBottom variant="h4" component="h3">
             The Pool
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-          Rent the ultimate pool area for your outdoor party! Reunions, receptions, or just have a blowout party! This is one of the most unique pools and venues in south Texas.
+          Rent the ultimate pool area for your outdoor party! This is one of the most unique pools and venues in south Texas.
         Come see for yourself!
           </Typography>
         </CardContent>
--- src/components/Main_Body/Wedding-card.jsx
+++ src/components/Main_Body/Wedding-card.jsx
@@ -1,54 +1,7 @@
-// import React from "react";
-// import "./style.css";
-// import Wedding2 from "../../util/images/pool4.jpg"
-// import Pool1 from "../../util/images/pool1.jpeg"
-// import ResizeImage from 'react-resize-image'
-// import {Card, Table} from 'reactstrap'
-// import Card from '@material-ui/core/Card';
-
-// const Wedding = ({show}) => {
-//     return (
-//         <div className="react-transition swipe-right m-5" style={{animationDuration: '1s', backgroundColor: '#bef1e9'}}>
-//         <Card>
-//             <div className="m-5">
-
-      
-//         <p>Rent the barn for your wedding! Beautiful rustic location that will give you an experince you'll never forget and cherish forever! </p>
-//         <img className="wedding m-5" src="https://cdn0.weddingwire.com/img_g/ww/t30_fairview-farm.jpg"
-//         alt="Wedding Venue"
-//         />
-
-//         <Table bordered>
-//         <thead>
-//   <tr>
-//     <th>Rates</th>
-//     <th>Hours</th> 
-//   </tr>
-//   </thead>
-//   <tr>
-//     <td>$1500</td>
-//     <td>Full Day</td> 
-//   </tr>
-//   <tr>
-//     <td>$750</td>
-//     <td>Half Day</td> 
-//   </tr>
-// </Table>
-
-// </div>
-// </Card>
-// </div>
-//     );
-// }
-
-
-
-// export default Wedding
 
 
 
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -57,7 +10,6 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import "./style.css";
-import Table from './Table'
 
 
 
@@ -66,8 +18,7 @@ function WeddingCard() {
 
 
   return (
-    // <div className="react-transition swipe-left m-5" style={{animationDuration: '1s'}}>
-    <Card raised='true' className="card react-transition swipe-left" style={{animationDuration: '1s'}}>
+    <Card raised='true' className="card react-transition swipe-left" style={{animationDuration: '2s'}}>
       <CardActionArea>
         <CardMedia
           className="media"
@@ -75,7 +26,7 @@ function WeddingCard() {
           title="Wedding Barn"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
+          <Typography gutterBottom variant="h4" component="h3">
             The Barn
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
